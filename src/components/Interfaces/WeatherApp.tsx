957bc9c4382f0166947e734c2fac7be21159c25c
Piotr Mazepka <piotrmazepka@gmail.com>
2022-07-29 10:32:46 +0200

    feat: Create WeatherApp to make api call

--- src/components/Interfaces/WeatherApp.tsx
+++ src/components/Interfaces/WeatherApp.tsx
@@ -1,3 +1,11 @@
 import { useState } from "react";
 import axios from 'axios';
 
+export const WeatherApp = () => {
+    const [data, setData] = useState<any>([]);
+    const [location, setLocation] = useState<string>('')
+
+
+    
+
+}
