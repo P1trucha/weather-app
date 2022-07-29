e91a53d7edb6351d5a9f3bf3955624e0d9987e36
Piotr Mazepka <piotrmazepka@gmail.com>
2022-07-29 10:41:22 +0200

    fix: Make api call using axios

--- src/components/Interfaces/WeatherApp.tsx
+++ src/components/Interfaces/WeatherApp.tsx
@@ -4,8 +4,16 @@ import axios from 'axios';
 export const WeatherApp = () => {
     const [data, setData] = useState<any>([]);
     const [location, setLocation] = useState<string>('')
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=0c38158b6b158d55212b2262c02aa404`;
 
-
-    
+    const searchLocation = (event: any) => {
+        if (event.key === 'Enter'){
+            axios.get(url).then((response)=>{
+                setData(response.data);
+                console.log(response.data);
+                setLocation('');
+            })
+        }
+    }
 
 }
