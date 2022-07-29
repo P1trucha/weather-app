30f38a6ddad34a5a40b2e091c650e93472eb2640
Piotr Mazepka <piotrmazepka@gmail.com>
2022-07-29 11:15:28 +0200

    fix: Create Interface for WeatherApp

--- src/components/Interfaces/WeatherApp.tsx
+++ src/components/Interfaces/WeatherApp.tsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import axios from 'axios';
+import {WeatherData} from './WeatherInterface';
+
 
 export const WeatherApp = () => {
-    const [data, setData] = useState<any>([]);
+    const [data, setData] = useState<WeatherData[]>([]);
     const [location, setLocation] = useState<string>('')
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=0c38158b6b158d55212b2262c02aa404`;
 
-    const searchLocation = (event: any) => {
+    const searchLocation = (event: React.KeyboardEvent<HTMLDivElement>) => {
         if (event.key === 'Enter'){
             axios.get(url).then((response)=>{
                 setData(response.data);
